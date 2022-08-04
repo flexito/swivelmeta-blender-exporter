206d37bb63e80c107ec82d3da7e023fe9425e958
Michael Beene <mike.beene@swivel.work>
2022-08-04 10:19:33 -0400

    Added resizing properties to Shopify Product Model

--- addons/io_swivelmeta_addon/components/definitions/shopify_product_model.py
+++ addons/io_swivelmeta_addon/components/definitions/shopify_product_model.py
@@ -1,3 +1,4 @@
+from bpy.props import FloatProperty, BoolProperty
 from ..swivelmeta_component import SwivelMetaComponent
 from ..types import Category, PanelType, NodeType
 
@@ -9,6 +10,18 @@ class ShopifyProductModel(SwivelMetaComponent):
         'category': Category.ECOMMERCE,
         'node_type': NodeType.NODE,
         'panel_type': [PanelType.OBJECT],
-        'icon': 'LINKED'
+        'icon': 'SCENE_DATA'
     }
 
+    resizeToFit: BoolProperty(
+        name="Resize To Fit",
+        description="Scale model to fit within given radius",
+        default=True
+    )
+
+    radius: FloatProperty(
+        name="Bounding Radius",
+        description="Maximum radius of model imported model",
+        default=1.0,
+        min=0.01
+    )
