4a8472c9b3bfd0228ae94909c0a645cad21abcf1
mark-fernandez <9023627+mfernandez4@users.noreply.github.com>
2023-02-13 14:33:04 -0800

    added mixer-visible

--- addons/io_swivelmeta_addon/components/definitions/mixer_visible.py
+++ addons/io_swivelmeta_addon/components/definitions/mixer_visible.py
@@ -4,6 +4,7 @@ from ..types import Category, PanelType, NodeType
 
 
 class MixerVisible(SwivelMetaComponent):
+    '''Mixer Visible Component'''
     _definition = {
         'name': 'mixer-visible',  # name of the A-Frame component
         'display_name': 'Mixer Visible',  # name displayed in Blender
@@ -12,5 +13,5 @@ class MixerVisible(SwivelMetaComponent):
         # panel in Blender where the component will be displayed
         'panel_type': [PanelType.OBJECT],
         # icon in Blender that will be displayed next to the component name
-        'icon': 'SHADING_SOLID'
+        'icon': 'IMAGE_ALPHA'
     }
