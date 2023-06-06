678610c7c23e70d8ab96981cb89425e8be4ed8ad
Michael Beene <mike.beene@swivel.work>
2023-06-06 13:14:44 -0400

    Add proximity and speed controls to teleport component

--- addons/io_swivelmeta_addon/components/definitions/teleport.py
+++ addons/io_swivelmeta_addon/components/definitions/teleport.py
@@ -1,6 +1,6 @@
 from ..swivelmeta_component import SwivelMetaComponent
 from ..types import Category, PanelType, NodeType
-from bpy.props import PointerProperty
+from bpy.props import PointerProperty, EnumProperty, FloatProperty
 from bpy.types import Object
 
 
@@ -18,4 +18,25 @@ class Teleport(SwivelMetaComponent):
         name="Destination",
         description="Destination Nav Point",
         type=Object
+    )
+
+    teleportSpeed: FloatProperty(
+        name="Teleport Speed",
+        description="Teleport speed in meters/second",
+        default=3.0,
+        min=0.0
+    )
+
+    triggerType: EnumProperty(
+        name="Trigger Type",
+        description="Select type of trigger",
+        items=[("PROXIMITY", "Proximity", "Will trigger when user gets close to this object"),
+               ("CLICKABLE", "Clickable", "Will trigger when user clicks this object")],
+        default="CLICKABLE")
+
+    proximityDistance: FloatProperty(
+        name="Proximity Distance",
+        description="Distance from object's origin before teleport is triggered",
+        default=1.0,
+        min=0.0
     )
